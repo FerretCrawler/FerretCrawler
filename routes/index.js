87b3998176a530c1e07aeead8251907550532e34
Thinh Phan <phanquocthinh@gmail.com>
2015-12-05 17:24:08 +0700

    Show index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
+
+router.use(bodyParser());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ferret Crawler', site: 'http://vnexpress.net' });
+});
+
+/* POST */
+router.post('/', function(req, res) {
+	res.send('Got a POST request');
 });
 
 module.exports = router;
